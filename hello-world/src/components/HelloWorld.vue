8aeb99c8b117aedd5cd041fe95dcdf68ca575fa4
Herodes <roadslocker@gmail.com>
2024-06-16 12:11:25 +0200

    initial commit : Vue Hello World Application

---
+++ hello-world/src/components/HelloWorld.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>{{ counter }}</p>
+    <button @click="incrementCounter">Increase Counter</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      counter: 0
+    };
+  },
+  methods: {
+    incrementCounter() {
+      this.counter++;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.hello {
+  text-align: center;
+  margin-top: 40px;
+}
+</style>
